183dc1f2c2c590cb2448e7bacd84f5463ee09ffe
Ed Deighton <eddeighton@hotmail.co.uk>
2023-10-21 05:46:34 +0100

    Refactored database out of the megastructure project into its own build system

---
+++ tests/basic/run_db_compiler.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/build/linux_gcc_shared_debug/db/install/bin/database_compiler_driver --api /workspace/root/src/database/src/tests/basic/database/api --src /workspace/root/src/database/src/tests/basic/database/src --stash ./stash --data ./database/data --input ./schema.dbs
